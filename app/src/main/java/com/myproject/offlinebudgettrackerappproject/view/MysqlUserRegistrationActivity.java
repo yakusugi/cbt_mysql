daff47f930ccbaead63234196c114eb9897eff37
yakusugi <yosukeito1987@gmail.com>
2024-10-27 11:27:04 +0900

    Oct 27 MysqlUserRegistrationActivity

--- app/src/main/java/com/myproject/offlinebudgettrackerappproject/view/MysqlUserRegistrationActivity.java
+++ app/src/main/java/com/myproject/offlinebudgettrackerappproject/view/MysqlUserRegistrationActivity.java
@@ -3,7 +3,7 @@ package com.myproject.offlinebudgettrackerappproject.view;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,22 +12,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.myproject.offlinebudgettrackerappproject.R;
 import com.myproject.offlinebudgettrackerappproject.dto.BudgetTrackerUserDto;
 import com.myproject.offlinebudgettrackerappproject.model.BudgetTrackerMySqlViewModel;
-import com.myproject.offlinebudgettrackerappproject.model.BudgetTrackerSpendingViewModel;
-import com.myproject.offlinebudgettrackerappproject.util.Callback;
 import com.myproject.offlinebudgettrackerappproject.util.LoginCallback;
 import com.myproject.offlinebudgettrackerappproject.util.SharedPreferencesManager;
 
-import androidx.fragment.app.Fragment;
-import android.content.SharedPreferences;
-
-public class MysqlLoginActivity extends AppCompatActivity {
+public class MysqlUserRegistrationActivity extends AppCompatActivity {
 
     EditText enterEmail, enterPassword;
     Button loginButton, registerButton;
@@ -36,22 +29,27 @@ public class MysqlLoginActivity extends AppCompatActivity {
 
     BudgetTrackerMySqlViewModel budgetTrackerMySqlViewModel;
 
-    Intent intent;
+    Intent i;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mysql_login);
+        setContentView(R.layout.activity_mysql_user_registration);
 
         enterEmail = (EditText) findViewById(R.id.mysql_login_email);
         enterPassword = (EditText) findViewById(R.id.mysql_login_password);
         loginButton = (Button) findViewById(R.id.mysql_login_btn);
         registerButton = (Button) findViewById(R.id.mysql_register_btn);
 
+        Log.d("ViewInitialization", "enterEmail: " + (enterEmail != null) +
+                ", enterPassword: " + (enterPassword != null) +
+                ", loginButton: " + (loginButton != null) +
+                ", registerButton: " + (registerButton != null));
+
         Log.d("LOGIN_TAG", "Views initialized");
 
-        budgetTrackerMySqlViewModel = new ViewModelProvider.AndroidViewModelFactory(MysqlLoginActivity.this.getApplication()).create(BudgetTrackerMySqlViewModel.class);
+        budgetTrackerMySqlViewModel = new ViewModelProvider.AndroidViewModelFactory(MysqlUserRegistrationActivity.this.getApplication()).create(BudgetTrackerMySqlViewModel.class);
 
         Log.d("LOGIN_TAG", "onCreate: ViewModel and views initialized");
 
@@ -88,25 +86,25 @@ public class MysqlLoginActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess() {
                         Log.d("LOGIN_TAG", "Login successful");
-                        Toast.makeText(MysqlLoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MysqlUserRegistrationActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
 
                         //Share and contain user email info
                         SharedPreferences sharedPref = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPref.edit();
                         editor.putString("user_email", email);
-                        SharedPreferencesManager.saveUserEmail(MysqlLoginActivity.this, email);
+                        SharedPreferencesManager.saveUserEmail(MysqlUserRegistrationActivity.this, email);
                         editor.apply();
 
                         // Navigate to another activity if needed
-                        intent = new Intent(view.getContext(), MysqlMainActivity.class);
-                        startActivity(intent);
+                        i = new Intent(view.getContext(), MysqlMainActivity.class);
+                        startActivity(i);
 //                        replaceFragment(new MyqlDashboardFragment());
                     }
 
                     @Override
                     public void onFailure(String errorMessage) {
                         Log.d("LOGIN_TAG", "Login failed: " + errorMessage);
-                        Toast.makeText(MysqlLoginActivity.this, "Login failed: " + errorMessage, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MysqlUserRegistrationActivity.this, "Login failed: " + errorMessage, Toast.LENGTH_SHORT).show();
                     }
                 });
             }
@@ -115,10 +113,52 @@ public class MysqlLoginActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("LOGIN_TAG", "Registration button clicked");
+                Log.d("LOGIN_TAG", "Login button clicked");
 
-                intent = new Intent(view.getContext(), MysqlUserRegistrationActivity.class);
-                startActivity(intent);
+                String email = enterEmail.getText().toString();
+                String password = enterPassword.getText().toString();
+
+                Log.d("LOGIN_TAG", "Email: " + email + ", Password: " + password);
+
+//                if (email.isEmpty()) {
+//                    enterEmail.setTextColor(Color.BLUE);
+//                    enterEmail.setError("Email cannot be empty");
+//                    return;
+//                }
+//
+//                if (password.isEmpty()) {
+//                    enterEmail.setTextColor(Color.RED);
+//                    enterPassword.setError("Password cannot be empty");
+//                    return;
+//                }
+
+                budgetTrackerUserDto = new BudgetTrackerUserDto(email, password);
+
+                budgetTrackerMySqlViewModel.login(budgetTrackerUserDto, new LoginCallback() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d("LOGIN_TAG", "Login successful");
+                        Toast.makeText(MysqlUserRegistrationActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        //Share and contain user email info
+                        SharedPreferences sharedPref = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString("user_email", email);
+                        SharedPreferencesManager.saveUserEmail(MysqlUserRegistrationActivity.this, email);
+                        editor.apply();
+
+                        // Navigate to another activity if needed
+                        i = new Intent(view.getContext(), MysqlMainActivity.class);
+                        startActivity(i);
+//                        replaceFragment(new MyqlDashboardFragment());
+                    }
+
+                    @Override
+                    public void onFailure(String errorMessage) {
+                        Log.d("LOGIN_TAG", "Login failed: " + errorMessage);
+                        Toast.makeText(MysqlUserRegistrationActivity.this, "Login failed: " + errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
